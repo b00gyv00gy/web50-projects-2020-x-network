457765099cee2faeceb358b448c98a54097bdf88
b00gyv00gy <anton.trushin@mail.ru>
2021-08-02 17:03:37 +0300

    made edit button

--- network/static/network/network.js
+++ network/static/network/network.js
@@ -1,14 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    document.querySelector('#following').addEventListener('click', hide_new_post);
-    document.querySelector('#all_posts').addEventListener('click', show_new_post);
+    let buttons = document.querySelectorAll("button")
+    
+    buttons.forEach(element => {
+        
+        element.addEventListener("click", function(){
+            
+            element.closest("div").querySelector("textarea").removeAttribute("readonly")
 
-  });
-
-  function hide_new_post() {
-    document.querySelector('#new_post').style.display = 'none'
-  }
-
-  function show_new_post() {
-    document.querySelector('#new_post').style.display = 'block'
-  }
+        });
+    });
+})
